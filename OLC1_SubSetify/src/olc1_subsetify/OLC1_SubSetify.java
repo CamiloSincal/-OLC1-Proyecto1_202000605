f8d7fbe81ea01a0774e1f75f7ed1f9791860df80
KmiloZ <3107320340407@ingenieria.usac.edu.gt>
2023-12-13 05:09:17 -0600

    JFlex y Cup
    Se hace la configuración inicial haciendo uso de JAR

---
+++ OLC1_SubSetify/src/olc1_subsetify/OLC1_SubSetify.java
@@ -0,0 +1,34 @@
+
+package olc1_subsetify;
+
+import java.io.FileInputStream;
+
+/**
+ * Clase principal de la aplicación
+ * @author Erick
+ */
+public class OLC1_SubSetify {
+
+    /**
+     * @param args argumentos de la linea de comando
+     */
+    public static void main(String[] args) {
+        interpretar("entrada.txt");
+    }
+    /**
+     * Método que interpreta el contenido del archivo que se encuentra en el path
+     * que recibe como parámentro
+     * @param path ruta del archivo a interpretar
+     */
+    private static void interpretar(String path) {
+        analizadores.Sintactico pars;
+        try {
+            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
+    
+}
